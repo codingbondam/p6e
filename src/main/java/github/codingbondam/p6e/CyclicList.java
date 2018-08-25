ec7e8fe8da0f1dec8d3c61d892ae4fae5f9a5586
codingbondam <codingbondam@github.com>
2018-08-25 16:30:15 -0500

    linked lists practice

---
+++ src/main/java/github/codingbondam/p6e/CyclicList.java
@@ -0,0 +1,55 @@
+package main.java.github.codingbondam.p6e;
+
+public class CyclicList {
+
+    public static class Node {
+        Node next;
+
+    }
+
+    public static boolean isCyclic(Node head) {
+
+        Node p1, p2;
+        p1 = p2 = head;
+
+        do {
+            p1 = p1.next;
+            p2 = p2.next == null ? null : p2.next.next;
+        } while (p1 != null && p2 != null && p1 != p2);
+
+        if (p1 == null || p2 == null) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Node n1 = new Node();
+        Node n2 = new Node();
+        Node n3 = new Node();
+        Node n4 = new Node();
+        Node n5 = new Node();
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n2;
+
+        // even case
+        System.out.println(isCyclic(n1));
+
+        n5.next = null;
+        System.out.println(isCyclic(n1));
+
+        // odd case
+        Node n6 = new Node();
+        n5.next = n6;
+        n6.next = n2;
+        System.out.println(isCyclic(n1));
+
+    }
+}
---
+++ src/main/java/github/codingbondam/p6e/MultiLevelList.java
@@ -0,0 +1,178 @@
+package main.java.github.codingbondam.p6e;
+
+public class MultiLevelList <T> {
+
+    Node<T> head;
+
+    Node<T> tail;
+
+    public void flatten() {
+
+        Node<T> curr = head;
+        Node<T> end = tail;
+
+        while (curr != null) {
+            if (curr.child != null) {
+                end.next = curr.child;
+                curr.child.prev = end;
+                end = curr.child;
+                while (end.next != null) {
+                    end = end.next;
+                }
+            }
+            curr = curr.next;
+        }
+    }
+
+    public void unflatten() {
+        unflattenInternal(head);
+        // alternate method involves traversing the flattened list back using two pointers
+        // this method visits nodes twice in some instances
+    }
+
+    public void unflattenInternal(Node<T> curr) {
+        while (curr != null) {
+            System.out.println("curr is " + curr.data);
+            if (curr.child != null) {
+                if (curr.child.prev != null) {
+                    curr.child.prev.next = null;
+                    curr.child.prev = null;
+                }
+                unflattenInternal(curr.child);
+            }
+            curr = curr.next;
+        }
+    }
+
+    public static class Node <T> {
+
+        T data;
+
+        Node<T> next;
+        Node<T> prev;
+        Node<T> child;
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+    }
+
+    public static void testFlatten1() {
+
+        MultiLevelList<String> list = new MultiLevelList<>();
+
+        Node<String> a = new Node("a");
+        Node<String> b = new Node("b");
+        Node<String> c = new Node("c");
+        Node<String> d = new Node("d");
+        Node<String> e = new Node("e");
+        Node<String> f = new Node("f");
+        Node<String> g = new Node("g");
+        Node<String> h = new Node("h");
+        Node<String> i = new Node("i");
+        Node<String> j = new Node("j");
+        Node<String> k = new Node("k");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        g.next = h;
+
+        a.child = f;
+        c.child = g;
+        g.child = j;
+        h.child = k;
+        e.child = i;
+
+        list.head = a;
+        list.tail = e;
+
+        list.flatten();
+
+        Node<String> curr = list.head;
+
+        StringBuffer buffer = new StringBuffer();
+        while (curr != null) {
+            buffer.append(curr.data);
+            curr = curr.next;
+        }
+        String output = buffer.toString();
+
+        assert "abcdefghijk".equals(output);
+    }
+
+    public static void testFlatten2() {
+
+        MultiLevelList<Integer> list = new MultiLevelList<>();
+
+        Node<Integer> n5 = new Node(5);
+        Node<Integer> n33 = new Node(33);
+        Node<Integer> n17 = new Node(17);
+        Node<Integer> n2 = new Node(2);
+        Node<Integer> n1 = new Node(1);
+        n5.next = n33; n33.next = n17; n17.next = n2; n2.next = n1;
+        n33.prev = n5; n17.prev = n33; n2.prev = n17; n1.prev = n2;
+
+        Node<Integer> n6 = new Node(6);
+        Node<Integer> n25 = new Node(25);
+        Node<Integer> n6_2 = new Node(6);
+        n6.next = n25; n25.next = n6_2;
+        n25.prev = n6; n6_2.prev = n25;
+
+        Node<Integer> n2_2 = new Node(2);
+        Node<Integer> n7 = new Node(7);
+        n2_2.next = n7;
+        n7.prev = n2_2;
+
+        Node<Integer> n8 = new Node(8);
+
+        Node<Integer> n9 = new Node(9);
+
+        Node<Integer> n12 = new Node(12);
+        Node<Integer> n5_2 = new Node(5);
+        n12.next = n5_2;
+        n5_2.prev = n12;
+
+        Node<Integer> n7_2 = new Node(7);
+
+        Node<Integer> n21 = new Node(21);
+        Node<Integer> n3 = new Node(3);
+        n21.next = n3;
+        n3.prev = n21;
+
+        list.head = n5;
+        list.tail = n1;
+
+        n5.child = n6;
+        n2.child = n2_2;
+        n25.child = n8;
+        n6_2.child = n9;
+        n2_2.child = n12;
+        n9.child = n7_2;
+        n12.child = n21;
+
+        list.flatten();
+
+        Node<Integer> curr = list.head;
+
+        StringBuffer buffer = new StringBuffer();
+        while (curr != null) {
+            buffer.append(curr.data);
+            curr = curr.next;
+        }
+
+        String output = buffer.toString();
+
+        assert "5331721625627891257213".equals(output);
+        list.unflatten();
+    }
+
+    public static void main(String[] args) {
+        testFlatten1();
+        testFlatten2();
+    }
+
+}
