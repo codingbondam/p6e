5f2b5dbf01f62c7215e03ad76b70f0df0dd8a3a0
codingbondam <codingbondam@github.com>
2018-08-26 17:53:48 -0500

    array representation of bst

--- src/main/java/github/codingbondam/p6e/trees/BinarySearchTree.java
+++ src/main/java/github/codingbondam/p6e/trees/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package main.java.github.codingbondam.p6e.trees;
 
+import java.util.Arrays;
+
 import static main.java.github.codingbondam.p6e.Utils.assertEquals;
 
 public class BinarySearchTree {
@@ -8,6 +10,56 @@ public class BinarySearchTree {
 
     BinarySearchTree(Node root) {
         this.root = root;
+    }
+
+    public BinarySearchTree(int[] input) {
+        Node[] nodes = new Node[input.length];
+        for (int i = 0; i < input.length; i++) {
+            nodes[i] = new Node(input[i]);
+        }
+
+        for (int i = 0; (2 * i + 1) < input.length; i++) {
+            Node node = nodes[i];
+            node.left = nodes[2 * i + 1];
+            if (2 * i + 2 < input.length) {
+                node.right = nodes[2 * i + 2];
+            }
+        }
+        root = nodes[0];
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node n) {
+        if (n == null) {
+            return 0;
+        }
+        int lSize = size(n.left);
+        int rSize = size(n.right);
+        return lSize + rSize + 1;
+    }
+
+    public int[] array() {
+        int[] output = new int[size()];
+        array(root, output, -1);
+        return output;
+    }
+
+    private int array(Node node, int[] output, int last) {
+        if (node == null) {
+            return last;
+        }
+        last++;
+        output[last] = node.data;
+        if (node.left != null) {
+            last = array(node.left, output, last);
+        }
+        if (node.right != null) {
+            last = array(node.right, output, last);
+        }
+        return last;
     }
 
     public int lca(int n1, int n2) {
@@ -36,6 +88,51 @@ public class BinarySearchTree {
         Node(int data) {
             this.data = data;
         }
+    }
+
+    public static void testArray() {
+        Node n23 = new Node(23);
+        Node n48 = new Node(48);
+        Node n144 = new Node(144);
+        Node n12 = new Node(12);
+        Node n15 = new Node(15);
+        Node n17 = new Node(17);
+        Node n13 = new Node(13);
+        Node n96 = new Node(96);
+        Node n22 = new Node(22);
+
+        n23.left = n48;
+        n23.right = n144;
+
+        n48.left = n12;
+        n48.right = n15;
+
+        n144.left = n17;
+        n144.right = n13;
+
+        n12.right = n96;
+
+        n13.left = n22;
+
+        BinarySearchTree bst = new BinarySearchTree(n23);
+        int[] out = bst.array();
+        Arrays.sort(out);
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < out.length; i++) {
+            buffer.append("<");
+            buffer.append(out[i]);
+            buffer.append(">");
+        }
+        assertEquals("<12><13><15><17><22><23><48><96><144>", buffer.toString());
+        BinarySearchTree bst1 = new BinarySearchTree(out);
+        int[] out2 = bst1.array();
+        buffer = new StringBuffer();
+        for (int i = 0; i < out2.length; i++) {
+            buffer.append("<");
+            buffer.append(out2[i]);
+            buffer.append(">");
+        }
+        assertEquals("<12><13><17><96><144><22><15><23><48>", buffer.toString());
     }
 
     public static void testLca() {
@@ -69,6 +166,7 @@ public class BinarySearchTree {
 
     public static void main(String[] args) {
         testLca();
+        testArray();
     }
 
 }
